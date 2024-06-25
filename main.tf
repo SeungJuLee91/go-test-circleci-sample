72895948dc60024daa7717bd56ed8a3875aec330
SeungJuLee91 <168509333+SeungJuLee91@users.noreply.github.com>
2024-06-25 16:05:26 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 resource "aws_instance" "my_instance" { # ”타입” “terraform구성파일에서 참조할 이름”
   ami           = "ami-09d8ed8255877048d" # 원하는 AMI ID로 변경
-  instance_type = "t2.micro"
+  instance_type = "t3.micro"
 
   tags = {
     Name = "TerraformTest" # 실제 인스턴스 이름 
